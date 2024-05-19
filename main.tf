b73e65fc03e593ef6e825466bcba9f1d849fecbc
teste <paulo@dexty@gmail.com>
2024-05-19 20:06:24 +0000

    alteração de ec2
    Aqui é um template commit-msg

--- main.tf
+++ main.tf
@@ -10,10 +10,10 @@ variable "server_port" {
   default     = 80
 }
 
-resource "aws_instance" "teste_servidor" {
-  ami           = "ami-0af6e9042ea5a4e3e"
+resource "aws_launch_configuration" "teste_servidor" {
+  image_id      = "ami-0af6e9042ea5a4e3e"
   instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
+  security_groups = [aws_security_group.instance.id]
   
   user_data = <<-EOF
   #!/bin/bash
@@ -22,12 +22,34 @@ resource "aws_instance" "teste_servidor" {
               sudo systemctl start apache2
               sudo bash -c 'echo Hello World run on port ${var.server_port} > /var/www/html/index.html'
               EOF
-  
-  user_data_replace_on_change = true
-  
-  tags = {
-    Name = "UbuntuTeste"
-  }
+
+  lifecycle {
+		create_before_destroy = true
+   }
+}
+
+data "aws_vpc" "default" {
+	default = true
+}
+
+data "aws_subnets" "default" {
+	filter { 
+		name = "vpc-id"
+		values = [data.aws_vpc.default.id]
+	}
+}
+
+resource "aws_autoscaling_group" "teste_servidor" {
+	launch_configuration = aws_launch_configuration.teste_servidor.name
+	vpc_zone_identifier = data.aws_subnets.default.ids
+	
+	min_size = 2
+	max_size = 3
+	tag {
+		key = "Name"
+		value = "terraform-asg-example"
+		propagate_at_launch = true
+	}
 }
 
 resource "aws_security_group" "instance" {
